7f31f6fbea5e513071717336ecbaeb3520746ffd
ne0-d <dhruvbhati2001@gmail.com>
2024-03-07 16:54:56 +0530

    updated model for comment reply and removes author

--- news/models.py
+++ news/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-class Test(models.Model):
-    name = models.CharField()
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    url = models.TextField()
+    pubDate = models.DateTimeField("published_date")
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.CharField(max_length=250) 
+    pubDate = models.DateTimeField("published_date")
+    parentComment = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
